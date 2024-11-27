ee312d2f28f2ced04d94a6ef2e14a8fe6fb8cfe8
Olivia Buckley <olivia.buckley7898@gmail.com>
2024-11-27 11:30:18 +0000

    complete seeding process for users, property types, and properties

---
+++ db/utils.js
@@ -0,0 +1,17 @@
+const createRef = (insertedUsers) => {
+  const userRef = {};
+  insertedUsers.forEach((user) => {
+    const fullName = user.first_name + " " + user.surname;
+    userRef[fullName] = user.user_id;
+  });
+  return userRef;
+};
+
+const formatProperties = (properties, refObj) => {
+  return properties.map((property) => {
+    const hostId = refObj[property.host_name];
+    return { ...property, host_id: hostId };
+  });
+};
+
+module.exports = { createRef, formatProperties };
