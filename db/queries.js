30cef103c188a3c9f6bd08af04c5d746fdebac46
Olivia Buckley <olivia.buckley7898@gmail.com>
2024-11-29 16:09:54 +0000

    add seeding functionality for reviews and favourites tables with tests

--- db/queries.js
+++ db/queries.js
@@ -31,7 +31,7 @@ FOREIGN KEY (guest_id) REFERENCES users(user_id) ON DELETE CASCADE,
 FOREIGN KEY (property_id) REFERENCES properties(property_id));`;
 
 exports.createReviewsQuery = `CREATE TABLE reviews(
-reviews_id SERIAL PRIMARY KEY,
+review_id SERIAL PRIMARY KEY,
 property_id INT NOT NULL,
 guest_id INT NOT NULL, 
 rating INT NOT NULL,
--- db/utils.js
+++ db/utils.js
@@ -1,10 +1,18 @@
-const createRef = (insertedUsers) => {
+const createUserRef = (insertedUsers) => {
   const userRef = {};
   insertedUsers.forEach((user) => {
     const fullName = user.first_name + " " + user.surname;
     userRef[fullName] = user.user_id;
   });
   return userRef;
+};
+
+const createPropertyRef = (insertedProperties) => {
+  const propertyRef = {};
+  insertedProperties.forEach((property) => {
+    propertyRef[property.name] = property.property_id;
+  });
+  return propertyRef;
 };
 
 const formatProperties = (properties, userRef) => {
@@ -15,4 +23,30 @@ const formatProperties = (properties, userRef) => {
   });
 };
 
-module.exports = { createRef, formatProperties };
+const formatFavourites = (favourites, userRef, propertyRef) => {
+  return favourites.map((favourite) => {
+    const guestId = userRef[favourite.guest_name];
+    const propertyId = propertyRef[favourite.property_name];
+    delete favourite.guest_name;
+    delete favourite.property_name;
+    return { ...favourite, guest_id: guestId, property_id: propertyId };
+  });
+};
+
+const formatReviews = (reviews, userRef, propertyRef) => {
+  return reviews.map((review) => {
+    const guestId = userRef[review.guest_name];
+    const propertyId = propertyRef[review.property_name];
+    delete review.guest_name;
+    delete review.property_name;
+    return { ...review, guest_id: guestId, property_id: propertyId };
+  });
+};
+
+module.exports = {
+  createUserRef,
+  createPropertyRef,
+  formatProperties,
+  formatFavourites,
+  formatReviews,
+};
