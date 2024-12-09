6351b6f85beb9d151608f39b7877ec957bf9cf8a
Olivia Buckley <olivia.buckley7898@gmail.com>
2024-12-09 12:02:26 +0000

    add functionality to post a favourite

--- app/models.js
+++ app/models.js
@@ -234,10 +234,56 @@ async function removePropertyReview(id) {
   return deletedReview.rows[0];
 }
 
+async function addPropertyFavourite(id, propertyFavourite) {
+  const { guest_id } = propertyFavourite;
+
+  const propertyExists = await db.query(
+    "SELECT * FROM properties WHERE property_id = $1",
+    [id]
+  );
+
+  if (propertyExists.rows.length === 0) {
+    return Promise.reject({
+      status: 404,
+      msg: `property with ID ${id} not found`,
+    });
+  }
+
+  if (!guest_id) {
+    return Promise.reject({ status: 400, msg: "missing required guest_id" });
+  }
+
+  const guestCheck = await db.query(
+    "SELECT * FROM users WHERE user_id = $1 AND role = $2",
+    [guest_id, "guest"]
+  );
+  if (guestCheck.rows.length === 0) {
+    return Promise.reject({
+      status: 400,
+      msg: `guest with ID ${guest_id} not found or not a guest`,
+    });
+  }
+
+  const addedPropertyFavourite = await db.query(
+    `INSERT INTO favourites(
+    property_id, 
+    guest_id) 
+    VALUES(
+    $1, $2)
+    RETURNING favourite_id`,
+    [id, guest_id]
+  );
+  return {
+    msg: "property favourited successfully",
+    favourite_id: addedPropertyFavourite.rows[0].favourite_id,
+  };
+}
+
 module.exports = {
   fetchProperties,
   fetchPropertyById,
   fetchReviewsByPropertyId,
   addPropertyReview,
   removePropertyReview,
+  addPropertyFavourite,
 };
