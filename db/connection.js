31b1621058875aaa8ace937a8bc0d5e791ff46e8
Olivia Buckley <olivia.buckley7898@gmail.com>
2024-12-11 17:00:14 +0000

    host the db

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,24 @@
 const { Pool } = require("pg");
 
-const path = `${__dirname}/../.env.test`;
+const ENV = process.env.NODE_ENV || "dev";
 
-require("dotenv").config({ path });
+const pathToCorrectEnvFile = `${__dirname}/../.env.${ENV}`;
 
-if (!process.env.PGDATABASE) {
-  throw new Error("PGDATABASE not set");
+require("dotenv").config({
+  path: pathToCorrectEnvFile,
+});
+
+const config = {};
+
+if (ENV === "prod") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
 }
 
-module.exports = new Pool();
+const pool = new Pool(config);
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
+}
+
+module.exports = pool;
