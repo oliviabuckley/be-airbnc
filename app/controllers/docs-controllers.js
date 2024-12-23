712382498d2938911629f78e9979c42d07b790ba
Olivia Buckley <olivia.buckley7898@gmail.com>
2024-12-23 11:59:25 +0000

    reorganise files to fix css file issue

--- app/controllers/docs-controllers.js
+++ app/controllers/docs-controllers.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 const serveDocs = (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "..", "index.html"));
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 };
 
 module.exports = { serveDocs };
