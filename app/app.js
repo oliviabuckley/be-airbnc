cee89bca223bae8f2210c2b9352161e4089993c0
Olivia Buckley <olivia.buckley7898@gmail.com>
2024-12-23 12:08:11 +0000

    fix css file issue

--- app/app.js
+++ app/app.js
@@ -6,8 +6,11 @@ const usersRouter = require("./routes/users-routes");
 const reviewsRouter = require("./routes/reviews-routes");
 const favouritesRouter = require("./routes/favourites-routes");
 const docsRouter = require("./routes/docs-routes");
+const path = require("path");
 
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", docsRouter);
 
--- app/controllers/docs-controllers.js
+++ app/controllers/docs-controllers.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 const serveDocs = (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+  res.sendFile(path.resolve("public", "index.html"));
 };
 
 module.exports = { serveDocs };
